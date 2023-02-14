9ddac27e43facd63951abaf05a4bc572d5b400a2
Jero1304 <calogeroleto13@gmail.com>
2023-02-14 16:43:29 +0100

    ADD stamp member in DOM

--- js/app.js
+++ js/app.js
@@ -6,28 +6,27 @@ Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
 Scott Estrada	Developer	scott-estrada-developer.jpg
 Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
 */
-let member;
-
-
+console.log('Our Team');
 
 const memberTeam = [
     {
-        fName :'Wayne Barnett',
-        job :'Founder & CEO',
-        photo :'wayne-barnett-founder-ceo.jpg',
+        fName: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg',
     },
     {
-        fName :'Angela Caroll',
-        job :'Chief Editor',
-        photo :'angela-caroll-chief-editor.jpg',
+        fName: 'Angela Caroll',
+        job: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg',
     },
     {
-        fName :'Walter Gordon',
-        job :'Office Manage',
-        photo :'walter-gordon-office-manager.jpg',
+        fName: 'Walter Gordon',
+        job: 'Office Manage',
+        photo: 'walter-gordon-office-manager.jpg',
     },
 ]
 
+const teamMembersEle = document.getElementById('members-info')
 
 for (let i = 0; i < memberTeam.length; i++) {
     const currentMember = memberTeam[i];
@@ -35,6 +34,15 @@ for (let i = 0; i < memberTeam.length; i++) {
     const role = currentMember.job;
     const imageProfile = currentMember.photo;
 
-    console.log('name',firstName,'role',role,'image',imageProfile,'array', currentMember[i]);    
+    console.log('name', firstName, 'role', role, 'image', imageProfile, 'array', currentMember[i]);
+
+    const memeberTable = `
+                <tr>
+                    <td>${firstName}</td>
+                    <td>${role}</td>
+                    <td>${imageProfile}</td>
+                </tr>
+                `
+    teamMembersEle.innerHTML +=  memeberTable;
 }
 
